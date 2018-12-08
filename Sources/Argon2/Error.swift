34176e12f123a0d95246042ea1800dc90aa03b45
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-08 12:20:44 -0600

    Documented Argon2.Error struct

--- Sources/Argon2/Error.swift
+++ Sources/Argon2/Error.swift
@@ -1,7 +1,14 @@
 extension Argon2 {
+    
+    /// An error that can occur when hashing or verifying bytes.
     public struct Error: Swift.Error {
+        
+        /// The Argon 2 error code.
         public let code: Int32
         
+        /// Creates a new `Argon2.Error` instance.
+        ///
+        /// - Parameter code: The Argon 2 error code from the function that failed.
         public init(code: Int32) {
             self.code = code
         }
