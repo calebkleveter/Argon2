18e1b8d254e74eac6c610470f1cd7b93b8e8afb6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-08 14:55:08 -0600

    Added testHash and testVerify cases to Argon2Tests suite

--- Tests/Argon2Tests/Argon2Tests.swift
+++ Tests/Argon2Tests/Argon2Tests.swift
@@ -2,5 +2,18 @@ import XCTest
 @testable import Argon2
 
 final class Argon2Tests: XCTestCase {
-    static var allTests: [(String, (Argon2Tests) -> ()throws -> ())] = []
+    func testHash()throws {
+        _ = try XCTAssertNoThrow(Argon2.hash("password"))
+    }
+    
+    func testVerify()throws {
+        let hash = try Argon2.hash("password")
+        
+        try XCTAssertNoThrow(Argon2.verify("password", against: hash))
+    }
+    
+    static var allTests: [(String, (Argon2Tests) -> ()throws -> ())] = [
+        ("testHash", testHash),
+        ("testVerify", testVerify)
+    ]
 }
