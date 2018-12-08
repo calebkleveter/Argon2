e6d3d715b451c5ebf54002926f7099c0052ac740
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-08 17:05:55 -0600

    Added testHashBenchmark case to Argon2Tests suite

--- Tests/Argon2Tests/Argon2Tests.swift
+++ Tests/Argon2Tests/Argon2Tests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class Argon2Tests: XCTestCase {
     func testHash()throws {
         _ = try XCTAssertNoThrow(Argon2.hash("password"))
+        try XCTAssertNotEqual(Argon2.hash("password"), "")
     }
     
     func testVerify()throws {
@@ -12,8 +13,19 @@ final class Argon2Tests: XCTestCase {
         try XCTAssertNoThrow(Argon2.verify("password", against: hash))
     }
     
+    func testHashBenchmark()throws {
+        measure {
+            do {
+                _ = try Argon2.hash([112, 97, 115, 115, 119, 111, 114, 100])
+            } catch let error {
+                XCTFail(error.localizedDescription)
+            }
+        }
+    }
+    
     static var allTests: [(String, (Argon2Tests) -> ()throws -> ())] = [
         ("testHash", testHash),
-        ("testVerify", testVerify)
+        ("testVerify", testVerify),
+        ("testHashBenchmark", testHashBenchmark)
     ]
 }
