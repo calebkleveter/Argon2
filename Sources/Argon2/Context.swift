663760566359ec087fc0b9a8e865161375d3bb2b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-08 12:09:36 -0600

    Completed Context properties, including .iterations, .memory, .parallelism, and .salt

--- Sources/Argon2/Context.swift
+++ Sources/Argon2/Context.swift
@@ -1,13 +1,30 @@
+import CArgon2
+
 extension Argon2 {
     public struct Context: Codable {
         public var mode: HashMode
+        public var iterations: UInt32
+        public var memory: UInt32
+        public var parallelism: UInt32
+        public var salt: Bytes
+        public var hashLength: UInt32
         
-        public init(mode: HashMode) {
+        public init(mode: HashMode, iterations: UInt32, memory: UInt32, parallelism: UInt32, salt: Bytes, hashLength: UInt32) {
             self.mode = mode
+            self.iterations = iterations
+            self.memory = memory
+            self.parallelism = parallelism
+            self.salt = salt
+            self.hashLength = hashLength
+        }
+        
+        public var saltLength: UInt32 {
+            return UInt32(self.salt.count)
         }
         
         public static var `default`: Context {
-            return Context(mode: .i)
+            let salt = (0...15).map { _ in return UInt8.random(in: 0...UInt8.max) }
+            return Context(mode: .i, iterations: 2, memory: 1<<16, parallelism: 1, salt: salt, hashLength: 32)
         }
     }
 }
